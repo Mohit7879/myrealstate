6fa4bbb54cb84f7c6c5b1d378bc7c1839fe97a78
mohit <mohityadavkkn24@gmail.com>
2023-11-06 09:50:58 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose =require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+
+mongoose.connect("mongodb+srv://mohityadav:kJUgEFqwrto0XUYW@realestatec.fupwuxe.mongodb.net/?retryWrites=true&w=majority")
+.then(()=> {
+    console.log("App is now connected to DB")
+}).catch((err)=> {
+    console.log(`${err}`);
+})
+
+   
+
+
 
 app.listen(3000,(err)=>{
     console.log('server running');
