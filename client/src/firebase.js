b8bf16b350f8f661fefb4b85b87636a1e91b1a5c
mohit <mohityadavkkn24@gmail.com>
2023-12-01 15:04:49 +0530

    changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
-  projectId:import.meta.env.projectId,
-  storageBucket: import.meta.env.storageBucket,
-  messagingSenderId:import.meta.env.messagingSenderId,
-  appId:import.meta.env.appId
+  authDomain: "realestate-d124d.firebaseapp.com",
+  projectId: "realestate-d124d",
+  storageBucket: "realestate-d124d.appspot.com",
+  messagingSenderId: "1050502617728",
+  appId: "1:1050502617728:web:33a3d9206029ef073ae465"
 };
 
 // Initialize Firebase
