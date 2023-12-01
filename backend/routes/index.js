1d5a8a96ab18dc367be24cdfec0fb55138a21724
mohit <mohityadavkkn24@gmail.com>
2023-12-01 18:40:53 +0530

    made changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,10 +3,5 @@ const homeimage=require('../models/homeimages.js')
 router.use('/api/user',require('./user.route'))
 router.use('/api/auth',require('./auth.route'))
 router.use('/api/listing',require('./listing.route'))
-router.get('/',(req,res)=>{
-    res.json({
-        message:'hello world',
-    })
-})
 
 module.exports=router;
