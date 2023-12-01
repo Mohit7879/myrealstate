6a55ffe997ebb7c917457b6342e267b381de559a
mohit <mohityadavkkn24@gmail.com>
2023-12-01 17:14:12 +0530

    fixed issue

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
-import Listingcart from '../components/listingcart';
-import Listing from './Listing';
+import Listingcart from '../components/Listingcart';
+
 
 
 
